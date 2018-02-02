5146198a881fc421face5a60c8eb5fefa1ca9c19
ArminEden <eden@bit-basti.de>
2018-02-02 10:36:07 +0100

    added further tests - ongoing process

---
+++ tests/SeoHeroToolAnalysisAdminFunctionalTest.php
@@ -0,0 +1,73 @@
+<?php
+/*
+  The class SeoHeroToolAnalysisAdminTest run checks against the SeoHeroToolAnalysisAdmin if a page is needed for the check.
+  All tests where no page is needed will be handled in the SeoHeroToolAnalysisAdminTest Class.
+ */
+class SeoHeroToolAnalysisAdminFunctionalTest extends FunctionalTest
+{
+    protected static $fixture_file = 'SeoHeroToolAnalysisAdminFunctionalTest.yml';
+    public static $use_draft_site = true;
+
+    /**
+    * Call protected/private method of a class.
+    *
+    * @param object &$object    Instantiated object that we will run method on.
+    * @param string $methodName Method name to call
+    * @param array  $parameters Array of parameters to pass into method.
+    *
+    * @return mixed Method return.
+    */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    /*
+      Test case for checkMeta Function. Checks the return value of this function for a normal case and an empty Meta Description.
+     */
+    public function testCheckMeta()
+    {
+        $Page = $this->objFromFixture('Page', 'home');
+        $Page->MetaDescription = 'This is a Test Case';
+        $seotest = new SeoHeroToolAnalysisAdmin();
+        $return =  $this->invokeMethod($seotest, 'checkMeta', [$Page]);
+
+        $this->assertTrue(is_array($return) && array_key_exists('UnsortedListEntries', $return), "Return checkMeta is not an array");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find checkMeta Description within the returned value");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find checkMeta Icon within the returned value");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find checkMeta HelpLink within the returned value");
+
+        $Page->MetaDescription = '';
+        $return = $this->invokeMethod($seotest, 'checkMeta', [$Page]);
+        $this->assertTrue(is_array($return) && array_key_exists('UnsortedListEntries', $return), "Return checkMeta is not an array for an empty MetaDescription.");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find checkMeta Description within the returned value for an empty MetaDescription.");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find checkMeta Icon within the returned value for an empty MetaDescription.");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find checkMeta HelpLink within the returned value for an empty MetaDescription.");
+    }
+
+    /*
+      Test case for checkURL Function. Checking home and someURL and that there is an array response.
+     */
+    public function testCheckURL()
+    {
+        $Page = $this->objFromFixture('Page', 'home');
+        $seotest = new SeoHeroToolAnalysisAdmin();
+        $return = $this->invokeMethod($seotest, 'checkURL', [$Page]);
+
+        $this->assertTrue(is_array($return) && array_key_exists('UnsortedListEntries', $return), "Return checkURL is not an array for home url");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find checkURL Description within the returned value for home url");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find checkURL Icon within the returned value for home url");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find checkURL HelpLink within the returned value for home url");
+
+        $Page = $this->objFromFixture('Page', 'someURL');
+        $return = $this->invokeMethod($seotest, 'checkURL', [$Page]);
+
+        $this->assertTrue(is_array($return) && array_key_exists('UnsortedListEntries', $return), "Return checkURL is not an array");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find checkURL Description within the returned value");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find checkURL Icon within the returned value");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find checkURL HelpLink within the returned value");
+    }
+}
--- tests/SeoHeroToolAnalysisAdminTest.php
+++ tests/SeoHeroToolAnalysisAdminTest.php
@@ -1,4 +1,8 @@
 <?php
+/*
+  The class SeoHeroToolAnalysisAdminTest run checks against the SeoHeroToolAnalysisAdmin if no page is needed for the check.
+  All tests where a page is needed will be handled in the SeoHeroToolAnalysisAdminFunctionalTest Class.
+ */
 class SeoHeroToolAnalysisAdminTest extends SapphireTest
 {
     /**
@@ -20,7 +24,7 @@ class SeoHeroToolAnalysisAdminTest extends SapphireTest
 
 
     /*
-      Test for function checkTitle. Tests that this returns an array with the correct datas.
+      Test for function checkTitle. Tests that this returns an array with the correct datas. Test the return for a normal case and an empty title.
      */
     public function testCheckTitle()
     {
@@ -31,10 +35,17 @@ class SeoHeroToolAnalysisAdminTest extends SapphireTest
         $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find Title Description");
         $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find Title Icon");
         $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find Title HelpLink");
+
+        $seotest->pageTitle = '';
+        $return = $this->invokeMethod($seotest, 'checkTitle');
+        $this->assertTrue(is_array($return) && array_key_exists('UnsortedListEntries', $return), "Return checkTitle is not an array for an empty Pagetitle");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find Title Description for an empty Pagetitle");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find Title Icon for an empty Pagetitle");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find Title HelpLink for an empty Pagetitle");
     }
 
     /*
-      Test for the function updateRules. Checks that this functions counts correct.
+      Test for the function updateRules. Checks that this functions counts correct. Test the return for different cases and also the combined result.
      */
     public function testUpdateRules()
     {
@@ -56,5 +67,27 @@ class SeoHeroToolAnalysisAdminTest extends SapphireTest
         $this->assertTrue($seotest->rules['wrong'] == 1, "rules['wrong'] has not the expected value. Expected to be 1. Value is: ".$seotest->rules["wrong"]);
         $this->assertTrue($seotest->rules['notice'] == 1, "rules['notice'] has not the expected value. Expected to be 1. Value is: ".$seotest->rules['notice']);
         $this->assertTrue($seotest->rules['good'] == 1, "rules['good'] has not the expected value. Expected to be 1. Value is: ".$seotest->rules['good']);
+    }
+
+    /*
+      Test for the function checkWordCount. Check this functions with normal values, string and empty value.
+     */
+    public function testCheckWordCount()
+    {
+        $seotest = new SeoHeroToolAnalysisAdmin();
+        $seotest->wordCount = 20;
+        $return = $this->invokeMethod($seotest, 'checkWordCount');
+
+        $this->assertTrue(is_array($return) && array_key_exists('UnsortedListEntries', $return), "Return checkWordCount is not an array a cound of 20 words.");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find Title Description for 20 words in checkWordCount");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find Title Icon for 20 words in checkWordCount");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find Title HelpLink for 20 words in checkWordCount");
+
+        $seotest->wordCount = 0;
+        $return = $this->invokeMethod($seotest, 'checkWordCount');
+        $this->assertTrue(is_array($return) && array_key_exists('UnsortedListEntries', $return), "Return checkWordCount is not an array a cound of 0 words.");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->Content), "Cant find Title Description for 0 words in checkWordCount");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->IconMess), "Cant find Title Icon for 0 words in checkWordCount");
+        $this->assertTrue(isset($return['UnsortedListEntries']->first()->HelpLink), "Cant find Title HelpLink for 0 words in checkWordCount");
     }
 }
